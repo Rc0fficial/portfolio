aecd55027e3e42bde892e2914ffd3340c1cc69e3
rc <to.arsalanali@gmail.com>
2022-12-30 20:03:52 +0500

    grammer mistakes

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -250,7 +250,7 @@ export default function Home({ articles }) {
   return (
     <>
       <Head>
-        <title>JavaScript - Devloper</title>
+        <title>JavaScript - Developer</title>
         <meta
           name="description"
           content="I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms."
@@ -259,7 +259,7 @@ export default function Home({ articles }) {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Computer Scientist - JavaScript Devloper - React | Next
+            Computer Scientist - JavaScript Developer - React | Next
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             As a front-end web developer with 1.5 years of experience, I have a
