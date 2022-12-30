2d5f2fdc343af96df1fd5843b01ba20e92bf12f9
rc <to.arsalanali@gmail.com>
2022-12-30 19:12:18 +0500

    update on navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -145,13 +145,13 @@ function NavItem({ href, children }) {
         className={clsx(
           'relative block px-3 py-2 transition',
           isActive
-            ? 'text-teal-500 dark:text-teal-400'
-            : 'hover:text-teal-500 dark:hover:text-teal-400'
+            ? 'text-yellow-500 dark:text-yellow-400'
+            : 'hover:text-yellow-500 dark:hover:text-yellow-400'
         )}
       >
         {children}
         {isActive && (
-          <span className="absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0 dark:from-teal-400/0 dark:via-teal-400/40 dark:to-teal-400/0" />
+          <span className="absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-teal-500/0 via-yellow-500/40 to-yellow-500/0 dark:from-yellow-400/0 dark:via-yellow-400/40 dark:to-yellow-400/0" />
         )}
       </Link>
     </li>
