9c2330898ba29fbfa413164cdbb61c1fa187dc56
rc0fficial <to.arsalanali@gmail.com>
2024-05-22 01:13:34 +0500

    update

--- src/components/HomeComponent.jsx
+++ src/components/HomeComponent.jsx
@@ -19,6 +19,7 @@ function SocialLink({ className, href, children, icon: Icon }) {
     <li className={clsx(className, 'flex')}>
       <Link
         href={href}
+        
         className="group flex text-sm font-medium text-zinc-800 transition hover:text-yellow-500 dark:text-zinc-200 dark:hover:text-yellow-500"
       >
         <Icon className="h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-yellow-500" />
@@ -96,7 +97,7 @@ export const HomeComponent = () => {
               <SocialLink href="#" icon={GitHubIcon} className="mt-4">
                 Follow on GitHub
               </SocialLink>
-              <SocialLink href="#" icon={LinkedInIcon} className="mt-4">
+              <SocialLink href="https://www.linkedin.com/in/rc0fficial/"  icon={LinkedInIcon} className="mt-4">
                 Follow on LinkedIn
               </SocialLink>
               <SocialLink
