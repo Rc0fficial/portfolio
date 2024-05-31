e969e8b76862c9e90ca6141cceff6f647e9c502c
rc0fficial <to.arsalanali@gmail.com>
2024-05-31 05:55:50 -0700

    google-analytics

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,23 +1,33 @@
-import { useEffect, useRef } from 'react'
-
-import { Footer } from '@/components/Footer'
-import { Header } from '@/components/Header'
-
-import '@/styles/tailwind.css'
-import 'focus-visible'
+import { useEffect, useRef } from 'react';
+import { useRouter } from 'next/router';
+import { Footer } from '@/components/Footer';
+import { Header } from '@/components/Header';
+import '@/styles/tailwind.css';
+import 'focus-visible';
+import { initGA, logPageView } from '@/utils/ga'; // Import the GA utilities
 
 function usePrevious(value) {
-  let ref = useRef()
+  let ref = useRef();
 
   useEffect(() => {
-    ref.current = value
-  }, [value])
+    ref.current = value;
+  }, [value]);
 
-  return ref.current
+  return ref.current;
 }
 
 export default function App({ Component, pageProps, router }) {
-  let previousPathname = usePrevious(router.pathname)
+  let previousPathname = usePrevious(router.pathname);
+  const nextRouter = useRouter();
+
+  useEffect(() => {
+    initGA();
+    logPageView();
+    nextRouter.events.on('routeChangeComplete', logPageView);
+    return () => {
+      nextRouter.events.off('routeChangeComplete', logPageView);
+    };
+  }, [nextRouter.events]);
 
   return (
     <>
@@ -34,5 +44,5 @@ export default function App({ Component, pageProps, router }) {
         <Footer />
       </div>
     </>
-  )
+  );
 }
