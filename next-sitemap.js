e790e53c964b0a44a768cc45b7ea5e659d789d88
Rc0fficial <to.arsalanali@gmail.com>
2024-06-02 22:00:50 -0700

    sitemap updated

---
+++ next-sitemap.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://www.arsalanalikhattak.me', // Your domain name
+    generateRobotsTxt: true, // Generate robots.txt file
+  };
+  
