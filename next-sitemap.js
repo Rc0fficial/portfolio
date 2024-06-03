539fb7b398eab6d840ca7435852391d764cbd7f5
Rc0fficial <to.arsalanali@gmail.com>
2024-06-03 08:50:45 -0700

    build err fixed

--- next-sitemap.js
+++
@@ -1,9 +0,0 @@
-const { generateSitemap } = require('next-sitemap');
-
-generateSitemap({
-  siteUrl: 'https://www.arsalanalikhattak.me',
-}).then(() => {
-  console.log('Sitemap generated successfully.');
-}).catch((err) => {
-  console.error('Error generating sitemap:', err);
-});
